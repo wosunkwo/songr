37129c1f54a7a8482cc748428ddfd00b8c05d5e8
williams osunkwo <wosunkwo@gmail.com>
2019-06-13 12:16:57 -0700

    completed the lab

---
+++ src/main/java/com/osunkwo/code401d4/day12/firstWebApp/Album.java
@@ -0,0 +1,49 @@
+package com.osunkwo.code401d4.day12.firstWebApp;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String title;
+    String artist;
+    int songCount;
+    int length;
+    String imageUrl;
+
+    public Album(){}
+
+    public Album(String title, String artist, int songCount, int length, String imageUrl){
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+}
