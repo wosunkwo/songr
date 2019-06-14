e1b566d5b6099481c097e870a0dbb63d1ee590b4
williams osunkwo <wosunkwo@gmail.com>
2019-06-14 14:51:32 -0700

    removed redundant code

--- src/test/java/com/osunkwo/code401d4/day12/firstWebApp/HelloWorldControllerTest.java
+++ src/test/java/com/osunkwo/code401d4/day12/firstWebApp/HelloWorldControllerTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,7 +13,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
